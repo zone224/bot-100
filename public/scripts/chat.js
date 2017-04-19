e7093250c3356292fa5f93ae9a3eeef0347b47f5
Rabah Zeineddine <Rabah@Rabahs-MacBook-Pro.local>
2017-04-19 10:27:48 -0300

    Changed to jquery

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,11 +1,29 @@
-var isOpen = false;
-function popupToggle(){
-    var popup = document.getElementById("chat-popup");
-    if(isOpen){
-        popup.style.animationName = "popup_close";
-        isOpen = false;
-    }else{
-        popup.style.animationName = "popup_open";
-        isOpen = true;
-    }
-}
+//var isOpen = false;
+//function popupToggle(){
+//    var popup = document.getElementById("chat-popup");
+//    if(isOpen){
+//        popup.style.animationName = "popup_close";
+//        isOpen = false;
+//    }else{
+//        popup.style.animationName = "popup_open";
+//        isOpen = true;
+//    }
+//}
+
+$(document).ready(function () {
+    var isOpen = false;
+    $('.chat-header').click(function () {
+        if (isOpen) {
+            $('.chat-popup').css({
+                "animation-name": "popup_close"
+            });
+            isOpen = false;
+        }
+        else {
+            isOpen = true;
+            $('.chat-popup').css({
+                "animation-name": "popup_open"
+            });
+        }
+    });
+});
