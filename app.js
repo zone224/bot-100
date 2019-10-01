7c298b579c62739d2807ff0102c7d12a2b9273ca
Sefi <jfariza1997@gmail.com>
2019-10-01 14:16:43 +0200

    arrow fucntions added

--- app.js
+++ app.js
@@ -2,58 +2,58 @@
  * Module dependencies.
  */
 
-var express = require('express'),
-    routes = require('./routes'),
-    http = require('http'),
-    path = require('path'),
-    fs = require('fs');
-var cfenv = require('cfenv');
+var express = require("express"),
+  routes = require("./routes"),
+  http = require("http"),
+  path = require("path"),
+  fs = require("fs");
+var cfenv = require("cfenv");
 
-var chatbot = require('./config/bot.js');
-
+var chatbot = require("./config/bot.js");
 
 var app = express();
 
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
-var errorHandler = require('errorhandler');
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var errorHandler = require("errorhandler");
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
-app.use(bodyParser.urlencoded({
+app.set("port", process.env.PORT || 3000);
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
+app.use(
+  bodyParser.urlencoded({
     extended: true
-}));
+  })
+);
 app.use(bodyParser.json());
 app.use(methodOverride());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/style', express.static(path.join(__dirname, '/views/style')));
-
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/style", express.static(path.join(__dirname, "/views/style")));
 
 // =====================================
 // WATSON CONVERSATION FOR ANA =========
 // =====================================
-app.post('/api/watson', function (req, res) {
-    processChatMessage(req, res);
+app.post("/api/watson", (req, res) => {
+  processChatMessage(req, res);
 }); // End app.post 'api/ana'
 function processChatMessage(req, res) {
-    chatbot.sendMessage(req, function (err, data) {
-        if (err) {
-            console.log("Error in sending message: ", err);
-            res.status(err.code || 500).json(err);
-        }
-        else {
-            var context = data.context;
-            res.status(200).json(data);
-        }
-    });
+  chatbot.sendMessage(req, (err, data) => {
+    if (err) {
+      console.log("Error in sending message: ", err);
+      res.status(err.code || 500).json(err);
+    } else {
+      var context = data.context;
+      res.status(200).json(data);
+    }
+  });
 }
 
-app.get('/', function(req,res){ res.render('chat.html')})
+app.get("/", (req, res) => {
+  res.render("chat.html");
+});
 
-
-http.createServer(app).listen(app.get('port'), '0.0.0.0', function() {
-    console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get("port"), "0.0.0.0", () => {
+  console.log("Express server listening on port " + app.get("port"));
 });
